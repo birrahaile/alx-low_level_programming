a729f64f9389f956fde09a59df541d9bd6f83bff
birrahaile <birrahaile@gmail.com>
2022-07-05 21:54:33 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,12 +2,6 @@
 #include "main.h"
 
 /**
- * jack+bauer - Check the code
- * Description: rfunction that prints every minute of
- * the day of Jack Bauer, starting from 00:00 to 23:59.
- * Return: Nothing.
- */
-/**
   * jack_bauer - Prints every minute of the day of Jack Bauer
   *
   * Return: ...
