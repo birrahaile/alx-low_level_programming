a811e59498cf3ede17368d2549eb1dc8d72233ff
birrahaile <birrahaile@gmail.com>
2022-07-05 21:26:19 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,14 +2,21 @@
 #include "main.h"
 
 /**
- * int _abs - Check the code
- * Description: return 1 if c is lowercase otherwise return 0
- * Return: The absolute value of a number.
- */
-int _abs(int r)
+  * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+int _abs(int c)
 {
-    if (r >= 0)
-		return (r);
-	else
-		return (r * -1);
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+
+	return (c);
 }
