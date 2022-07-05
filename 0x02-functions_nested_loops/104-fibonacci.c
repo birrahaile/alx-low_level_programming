af3bd3db6be91d824ddb5cebb8c49d678ac2aecc
birrahaile <birrahaile@gmail.com>
2022-07-05 23:06:13 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
+#define FIB_MAX 98
+#define MAXVAL 100000000000
 
 /**
-  * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2
-  * The numbers are separated by comma
-  *
-  * Return: Nothing!
-  */
+ * main - print first 98 fibonacci numbers
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, sum = k;
+	int count;
+	unsigned long int n1a, n1b, n2a, n2b, tmpa, tmpb;
 
-	while (k + j < 98)
+	n1a = n2a = tmpa = tmpb = 0;
+	n1b = 1;
+	n2b = 2;
+
+	if (n1a)
+		printf("%lu%0*lu", n1a, 11, n1b);
+	else
+		printf("%lu", n1b);
+
+	for (count = 1; count < FIB_MAX; count++)
 	{
-		k += j;
-
-		if (k % 2 == 0)
-			sum += k;
-
-		j = k - j;
-
-		++i;
+		printf(", ");
+		if (n2a)
+			printf("%lu%0*lu", n2a, 11, n2b);
+		else
+			printf("%lu", n2b);
+		tmpa = n1a;
+		tmpb = n1b;
+		n1a = n2a;
+		n1b = n2b;
+		n2a += tmpa;
+		n2b += tmpb;
+		n2a += n2b / MAXVAL;
+		n2b %= MAXVAL;
 	}
-
-	printf("%ld\n", sum);
+	printf("\n");
 	return (0);
 }
